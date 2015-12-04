092445956a61d6aae7879c1fbf767363a7d623d3
Ethan Sattler <ethan@10.39.48.172>
2015-12-04 15:34:08 -0500

    Added comments to HttpMethod.java

--- src/main/java/org/hbw/espresso/http/HttpMethod.java
+++ src/main/java/org/hbw/espresso/http/HttpMethod.java
@@ -1,7 +1,8 @@
 package org.hbw.espresso.http;
 
 /**
- * Enum containing all HTTP/1.1 methods. HttpMethod.ACTION will match all Methods.
+ * Enum containing all HTTP/1.1 methods.
+ * HttpMethod.ACTION will match all HTTP/1.1 Methods.
  */
 public enum HttpMethod {
 	GET, HEAD, POST, PUT, DELETE, TRACE, OPTIONS, CONNECT, PATCH, ACTION
