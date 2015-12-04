3ba5d8b8c669b588e28c8db5aa2e4f36e765db19
Niles Rogoff <nilesrogoff@gmail.com>
2015-12-04 15:50:30 -0500

    Documented jetty logger

--- src/main/java/org/hbw/espresso/logging/EspressoJettyLogger.java
+++ src/main/java/org/hbw/espresso/logging/EspressoJettyLogger.java
@@ -14,49 +14,95 @@ import org.eclipse.jetty.util.log.JavaUtilLog;
 import org.eclipse.jetty.util.log.Logger;
 
 public class EspressoJettyLogger extends AbstractLogger {
+
 	/**
-	 * 
+	 * Instance variable dateFormat, which is written to each log line.
 	 */
-	DateFormat dateFormat;
-	List<OutputStream> loggingList = new ArrayList<>();
-	Boolean debug;
+	protected DateFormat dateFormat;
+	/**
+	 * Instance variable that contains all OutputStreams that should be logged
+	 * to.
+	 */
+	private final List<OutputStream> loggingList = new ArrayList<>();
+	/**
+	 * Instance variable that notes whether debug messages should be logged or
+	 * ignored.
+	 */
+	protected Boolean debug;
+	/**
+	 * Instance variable that contains the line ending. Defaults to \r\n.
+	 */
 	String endl;
 
+	/**
+	 * Takes a type and a message and generates a string containing the current
+	 * pid, time and date, type, message, then writes that string to every
+	 * OutputStream in loggingList.
+	 *
+	 * @param type
+	 * @param message
+	 */
 	private void logMessage(String type, String message) {
-		String built = String.format("[%s %s %s] %s", ManagementFactory.getRuntimeMXBean().getName(), this.dateFormat.format(new Date()), type, message);
-		for (OutputStream o : loggingList) {
+		String built = String.format("[%s %s %s] %s%s", ManagementFactory.getRuntimeMXBean().getName(), this.dateFormat.format(new Date()), type, message, endl);
+		loggingList.stream().forEach((OutputStream o) -> {
 			try {
 				o.write(built.getBytes());
 			} catch (IOException e) {
 				// We should probably do something about this
 			}
-		}
+		});
 	}
 
+	/**
+	 * Constructs a new EspressoJettyLogger with a default date format, line
+	 * ending and debug value. Also sets loggingList to the parameter files.
+	 *
+	 * @param files
+	 */
 	protected EspressoJettyLogger(List<OutputStream> files) {
 		this.dateFormat = new SimpleDateFormat("HH:mm:ss mm/dd/yyyy");
 		this.endl = "\r\n";
 		this.debug = false;
-		for (OutputStream s : files) {
-			loggingList.add(s);
-		}
+		loggingList.addAll(files);
 	}
 
+	/**
+	 * No idea what this does.
+	 *
+	 * @param string
+	 * @return
+	 */
 	@Override
 	protected Logger newLogger(String string) {
 		return new JavaUtilLog(string);
 	}
 
+	/**
+	 * Needed for Jetty API compatability.
+	 *
+	 * @return String "Jetty Logger for Espresso"
+	 */
 	@Override
 	public String getName() {
 		return "Jetty Logger for Espresso";
 	}
 
+	/**
+	 * Logs a warning with the message string
+	 *
+	 * @param string
+	 * @param os
+	 */
 	@Override
 	public void warn(String string, Object... os) {
 		logMessage("WARNING", string);
 	}
 
+	/**
+	 * Logs a warning from the exception e
+	 *
+	 * @param e
+	 */
 	@Override
 	public void warn(Throwable e) {
 		Arrays.asList(e.getStackTrace()).stream().forEachOrdered((x -> {
@@ -64,17 +110,34 @@ public class EspressoJettyLogger extends AbstractLogger {
 		}));
 	}
 
+	/**
+	 * Logs a warning using the string string then the exception thrwbl.
+	 *
+	 * @param string
+	 * @param thrwbl
+	 */
 	@Override
 	public void warn(String string, Throwable thrwbl) {
 		this.warn(string);
 		this.warn(thrwbl);
 	}
 
+	/**
+	 * Logs an info message with the string string
+	 *
+	 * @param string
+	 * @param os
+	 */
 	@Override
 	public void info(String string, Object... os) {
 		logMessage("INFO", string);
 	}
 
+	/**
+	 * Logs an info message from an exception e.
+	 *
+	 * @param e
+	 */
 	@Override
 	public void info(Throwable e) {
 		Arrays.asList(e.getStackTrace()).stream().forEachOrdered((x -> {
@@ -82,22 +145,44 @@ public class EspressoJettyLogger extends AbstractLogger {
 		}));
 	}
 
+	/**
+	 * Logs an info message using the string string than the exception thrwbl.
+	 *
+	 * @param string
+	 * @param thrwbl
+	 */
 	@Override
 	public void info(String string, Throwable thrwbl) {
 		this.info(string);
 		this.info(thrwbl);
 	}
 
+	/**
+	 * returns whether debug is enabled or not.
+	 *
+	 * @return boolean currentDebugValue
+	 */
 	@Override
 	public boolean isDebugEnabled() {
 		return this.debug;
 	}
 
+	/**
+	 * Sets debug to bln
+	 *
+	 * @param bln
+	 */
 	@Override
 	public void setDebugEnabled(boolean bln) {
 		this.debug = (Boolean) bln;
 	}
 
+	/**
+	 * Logs a debug message from the string string if debug is enabled
+	 *
+	 * @param string
+	 * @param os
+	 */
 	@Override
 	public void debug(String string, Object... os) {
 		if (this.debug) {
@@ -105,6 +190,11 @@ public class EspressoJettyLogger extends AbstractLogger {
 		}
 	}
 
+	/**
+	 * Logs a debug message from the exception e if debug is enabled.
+	 *
+	 * @param e
+	 */
 	@Override
 	public void debug(Throwable e) {
 		Arrays.asList(e.getStackTrace()).stream().forEachOrdered((x -> {
@@ -112,12 +202,23 @@ public class EspressoJettyLogger extends AbstractLogger {
 		}));
 	}
 
+	/**
+	 * Logs a debug message with the message string and the exception thrwbl.
+	 *
+	 * @param string
+	 * @param thrwbl
+	 */
 	@Override
 	public void debug(String string, Throwable thrwbl) {
 		this.debug(string);
 		this.debug(thrwbl);
 	}
 
+	/**
+	 * Does nothing because the Jetty Logging API is pants on head retarded
+	 *
+	 * @param e
+	 */
 	@Override
 	public void ignore(Throwable e) {
 	}
