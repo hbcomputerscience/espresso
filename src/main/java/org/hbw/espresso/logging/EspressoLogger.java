21ffd10169b7d91ed86b5b8b62b8c7fc1f607050
Niles Rogoff <nilesrogoff@gmail.com>
2015-12-04 09:07:54 -0500

    Fixed bug where logging messages before initializing would do nothing

--- src/main/java/org/hbw/espresso/logging/EspressoLogger.java
+++ src/main/java/org/hbw/espresso/logging/EspressoLogger.java
@@ -31,74 +31,74 @@ public class EspressoLogger {
 	}
 
 	public static void warn(String string, Object... os) {
-		instance.fmap(((x) -> {
+		EspressoLogger.getInstance().fmap(((x) -> {
 			x.warn(string, os);
 		}));
 	}
 
 	public static void warn(Throwable thrwbl) {
-		instance.fmap(((x) -> {
+		EspressoLogger.getInstance().fmap(((x) -> {
 			x.warn(thrwbl);
 		}));
 	}
 
 	public static void warn(String string, Throwable thrwbl) {
-		instance.fmap(((x) -> {
+		EspressoLogger.getInstance().fmap(((x) -> {
 			x.warn(string, thrwbl);
 		}));
 	}
 
 	public static void info(String string, Object... os) {
-		instance.fmap(((x) -> {
+		EspressoLogger.getInstance().fmap(((x) -> {
 			x.info(string, os);
 		}));
 	}
 
 	public static void info(Throwable thrwbl) {
-		instance.fmap(((x) -> {
+		EspressoLogger.getInstance().fmap(((x) -> {
 			x.info(thrwbl);
 		}));
 	}
 
 	public static void info(String string, Throwable thrwbl) {
-		instance.fmap(((x) -> {
+		EspressoLogger.getInstance().fmap(((x) -> {
 			x.info(string, thrwbl);
 		}));
 	}
 
 	public static Maybe<Boolean> isDebugEnabled() {
-		return instance.fmap(x -> {
+		return EspressoLogger.getInstance().fmap(x -> {
 			return x.debug;
 		});
 	}
 
 	public static void setDebugEnabled(Boolean bln) {
-		instance.fmap(x -> {
+		EspressoLogger.getInstance().fmap(x -> {
 			x.setDebugEnabled((boolean) bln);
 		});
 	}
 
 	public static void debug(String string, Object... os) {
-		instance.fmap(((x) -> {
+		EspressoLogger.getInstance().fmap(((x) -> {
 			x.debug(string, os);
 		}));
 
 	}
 
 	public static void debug(Throwable thrwbl) {
-		instance.fmap(((x) -> {
+		EspressoLogger.getInstance().fmap(((x) -> {
 			x.debug(thrwbl);
 		}));
 	}
 
 	public static void debug(String string, Throwable thrwbl) {
-		instance.fmap(((x) -> {
+		EspressoLogger.getInstance().fmap(((x) -> {
 			x.debug(string, thrwbl);
 		}));
 	}
 
 	public static void ignore(Throwable thrwbl) {
-		instance.fmap(((x) -> {
+		EspressoLogger.getInstance().fmap(((x) -> {
 			x.ignore(thrwbl);
 		}));
 	}
